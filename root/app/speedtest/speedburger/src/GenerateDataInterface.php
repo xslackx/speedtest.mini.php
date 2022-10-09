0f7b8d67607bfcdaac61849e393acccd3675af57
Ruan <xslackx@live.fr>
2022-10-09 11:58:37 -0300

    Removing repeated methods and adding comments

--- root/app/speedtest/speedburger/src/GenerateDataInterface.php
+++ root/app/speedtest/speedburger/src/GenerateDataInterface.php
@@ -1,27 +1,33 @@
 <?php
 
 /**
- * Generate a chunk data to response to client
+ * To create the random data, the values ​​in bytes
+ * are determined, generating the chunks for
+ * sending to the client.
  * @see https://github.com/adolfintel/speedtest/blob/master/garbage.php
+ * @see https://github.com/ipburger/speedtest.mini.php/blob/master/root/app/speedtest/random.php
  */
 
 interface GenerateDataInterface
 {
   /**
-   * Check limits and return
-   * @return
+   * The values ​​in bytes are determined from the size
+   * of the object of the $_GET method requested by the client,
+   * then the bytes that must be returned to the client are calculated
+   * @return string[] An array of integer values ​​containing the amount of data
+   * to send and create the data segments
    */
-   public function checkLimits();
-
-  /**
-   * Create Chunk data and return
-   * @return
-   */
-   public function createChunk();
+   public function bytesChunk($data);
 
    /**
-    * Stream Chunk data and return
-    * @return
+    * When we receive the array returned by the bytesChunk() method,
+    * execute the rndbytes binary because it has versatility in choosing
+    * which PRNGs (pseudo random number generators) to use, but if the system
+    * does not have it installed, the openssl_random_pseudo_bytes
+    * function will be used and the data will be returned to the client.
+    * @see https://linux.die.net/man/1/rndbytes
+    * @see https://www.php.net/manual/en/function.openssl-random-pseudo-bytes.php
+    * @return string Returns segmented data
     */
-    public function streamChunk();
+    public function streamChunk($bytes);
 }
